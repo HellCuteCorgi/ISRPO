199fb878bbacef40832f5e060dffbf897c47f486
HellCuteCorgi <maks.samoylov.04@bk.ru>
2022-09-22 22:58:04 +0300

    Library

---
+++ LR1/LR1/Class1.cs
@@ -0,0 +1,35 @@
+﻿namespace LR1
+{
+    public class Class1
+    {
+        public void addition(float x, float y)
+        {
+            float summ = x + y;
+            Console.Write(x + "+" + y + "=" + summ);
+            Console.WriteLine();
+        }
+
+        internal void subtraction(float x, float y)
+        {
+            float min = x - y;
+            Console.Write(x + "-" + y + "=" + min);
+            Console.WriteLine();
+
+        }
+
+        public void generation(float x, float y)
+        {
+            float gen = x * y;
+            Console.Write(x + "*" + y + "=" + gen);
+            Console.WriteLine();
+
+        }
+        internal void degree(float x, float y)
+        {
+            float deg = x / y;
+            Console.Write(x + "/" + y + "=" + deg);
+            Console.WriteLine();
+
+        }
+    }
+}
